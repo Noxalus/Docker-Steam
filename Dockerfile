3dcf3aa77552c7075a569fdd59c624b2e5b1631f
Noxalus <noxalus@gmail.com>
2020-08-05 18:56:13 +0200

    Add a script that will prepare the build before the upload to Steam.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM ubuntu:18.04
 
-COPY  ./scripts /home/scripts
+COPY ./content /home
 
 RUN set -x && \
     apt-get update -y && \
     apt-get install curl zip -y && \
+    apt-get install lib32gcc1 -y && \
     curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf - -C /home
---
+++ content/prepare_build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+set -ex
+
+if [ "$BUILD_TARGET" = "StandaloneWindows64" ]; then
+    STEAM_DEPOT_ID=$STEAM_WINDOWS_DEPOT_ID
+elif [ "$BUILD_TARGET" = "StandaloneOSX" ]; then
+    STEAM_DEPOT_ID=$STEAM_OSX_DEPOT_ID
+elif [ "$BUILD_TARGET" = "StandaloneLinux64" ]; then
+    STEAM_DEPOT_ID=$STEAM_LINUX_DEPOT_ID
+fi
+
+CONTENT_ROOT="/home/content"
+
+# Replace all values in Valve template files for the actual project
+APP_BUILD_FILENAME="app_build_$STEAM_APP_ID.vdf"
+# Change this according the platform
+DEPOT_BUILD_FILENAME="app_build_$STEAM_WINDOWS_DEPOT_ID.vdf"
+
+mv /home/scripts/app_build_1000.vdf /home/scripts/$APP_BUILD_FILENAME
+mv /home/scripts/depot_build_1001.vdf /home/scripts/$DEPOT_BUILD_FILENAME
+
+# App build file
+sed -i "s/1000/$STEAM_APP_ID/g" $APP_BUILD_FILENAME
+sed -i "s/1001/$STEAM_DEPOT_ID/g" $APP_BUILD_FILENAME
+sed -i "s/Your build description here/$CI_COMMIT_DESCRIPTION/g" $APP_BUILD_FILENAME
+
+# Depot build file
+sed -i "s/1001/$STEAM_DEPOT_ID/g" $DEPOT_BUILD_FILENAME
+sed -i "s/D:\\\MyGame\\\rel\\\master\\\/$CONTENT_ROOT/g" $DEPOT_BUILD_FILENAME
+
+# Move project build in the appropriate folder
+mv "./Builds/$BUILD_TARGET/$BUILD_NAME" $CONTENT_ROOT
