e4818138ebf82851216aedec6ff087d7090d1c3d
Noxalus <noxalus@gmail.com>
2020-08-19 14:20:03 +0200

    Update the run_build.sh to allow Steam DRM wrapping of game's binary.
    And updated the README file accordingly. Also added checks for env variables in both run_build.sh and prepare_build.sh scripts.

--- content/prepare_build.sh
+++ content/prepare_build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -ex
+set -e
 
 CONTENT_ROOT="/home/content"
 SCRIPTS_ROOT="/home/scripts"
@@ -10,19 +10,63 @@ APP_BUILD_FILENAME="$SCRIPTS_ROOT/app_build.vdf"
 # Change this according the platform
 DEPOT_BUILD_FILENAME="$SCRIPTS_ROOT/depot_build.vdf"
 
-# Escape build description
-BUILD_DESCRIPTION=$(printf '%s\n' "$STEAM_DESCRIPTION" | sed -e 's/[\/&]/\\&/g')
+# Check needed variables
+
+if [ ! $STEAM_APP_ID ]
+then
+    echo "STEAM_APP_ID env variable not found."
+    exit 1
+fi
+
+if [ ! $STEAM_DEPOT_ID ]
+then
+    echo "STEAM_DEPOT_ID env variable not found."
+    exit 1
+fi
+
+if [ ! $BUILD_FOLDER ]
+then
+    echo "BUILD_FOLDER env variable not found."
+    exit 1
+fi
+
+set -x
 
 # App build file
-sed -i 's|{APP_ID}|'"$STEAM_APP_ID"'|g' $APP_BUILD_FILENAME
-sed -i 's|{DESCRIPTION}|'"$BUILD_DESCRIPTION"'|g' $APP_BUILD_FILENAME
-sed -i 's|{BRANCH}|'"$STEAM_BRANCH"'|g' $APP_BUILD_FILENAME
-sed -i 's|{PREVIEW}|'"$STEAM_PREVIEW"'|g' $APP_BUILD_FILENAME
-sed -i 's|{DEPOT_ID}|'"$STEAM_DEPOT_ID"'|g' $APP_BUILD_FILENAME
+sed -i 's|{APP_ID}|'$STEAM_APP_ID'|g' $APP_BUILD_FILENAME
+sed -i 's|{DEPOT_ID}|'$STEAM_DEPOT_ID'|g' $APP_BUILD_FILENAME
+
+# Escape build description to avoid sed replacement issue
+if [ "$STEAM_DESCRIPTION" ]
+then
+    BUILD_DESCRIPTION=$(printf '%s\n' "$STEAM_DESCRIPTION" | sed -e 's/[\/&]/\\&/g')
+    sed -i 's|{DESCRIPTION}|'"$BUILD_DESCRIPTION"'|g' $APP_BUILD_FILENAME
+else
+    sed -i 's|{DESCRIPTION}|[NO DESCRIPTION PROVIDED]|g' $APP_BUILD_FILENAME
+fi
+
+if [ "$BRANCH" ]
+then
+    sed -i 's|{BRANCH}|'"$STEAM_BRANCH"'|g' $APP_BUILD_FILENAME
+else
+    sed -i 's|{BRANCH}||g' $APP_BUILD_FILENAME
+fi
+
+if [ $STEAM_PREVIEW ]
+then
+    sed -i 's|{PREVIEW}|'$STEAM_PREVIEW'|g' $APP_BUILD_FILENAME
+else
+    sed -i 's|{PREVIEW}|0|g' $APP_BUILD_FILENAME
+fi
 
 # Depot build file
-sed -i 's|{DEPOT_ID}|'"$STEAM_DEPOT_ID"'|g' $DEPOT_BUILD_FILENAME
-sed -i 's|{CONTENT_ROOT}|'"$CONTENT_ROOT"'|g' $DEPOT_BUILD_FILENAME
+sed -i 's|{DEPOT_ID}|'$STEAM_DEPOT_ID'|g' $DEPOT_BUILD_FILENAME
+sed -i 's|{CONTENT_ROOT}|'$CONTENT_ROOT'|g' $DEPOT_BUILD_FILENAME
 
 # Move project build in the appropriate folder
 cp -a "$BUILD_FOLDER/." $CONTENT_ROOT/
+
+set +x
+
+echo "Ready to upload the build to Steam!"
+echo "Launch the run_build.sh script when you are ready."
--- content/run_build.sh
+++ content/run_build.sh
@@ -1,7 +1,68 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Used to disable 2AF
-mv $STEAM_SSFN_FILE_PATH /root/Steam
+set -e
+
+CONTENT_ROOT="/home/content"
+STEAM_CMD_STDERR_FILE="/root/Steam/logs/stderr.txt"
+
+if [ ! $STEAM_ACCOUNT ]
+then
+    echo "STEAM_ACCOUNT env variable not found."
+    exit 1
+fi
+
+if [ ! $STEAM_PASSWORD ]
+then
+    echo "STEAM_PASSWORD env variable not found."
+    exit 1
+fi
+
+if [ ! $STEAM_SSFN_FILE_PATH ]
+then
+    echo "STEAM_SSFN_FILE_PATH env variable not found."
+    echo "It's needed to bypass the Steam's two-factor authentication."
+    exit 1
+fi
+
+set -x
+
+# Use SSFN file to disable 2AF
+cp "$STEAM_SSFN_FILE_PATH" /root/Steam
+
+set +x
+
+# Wrap binary using Steam DRM?
+if [ $STEAM_DRM_TOOL ]
+then
+    if [ ! $BINARY_PATH ]
+    then
+        echo "BINARY_PATH env variable not found."
+        exit 1
+    fi
+
+    DRM_FLAGS=$([ $STEAM_DRM_FLAGS ] && echo $STEAM_DRM_FLAGS || echo "0")
+
+    set -x
+
+    ABSOLUTE_BINARY_PATH="${CONTENT_ROOT}/${BINARY_PATH}"
+
+    # Request binary with DRM
+    /home/steamcmd.sh \
+        +login $STEAM_ACCOUNT $STEAM_PASSWORD \
+        +drm_wrap $STEAM_APP_ID "$ABSOLUTE_BINARY_PATH" "$ABSOLUTE_BINARY_PATH" $STEAM_DRM_TOOL $STEAM_DRM_FLAGS \
+        +quit
+fi
 
 # Upload the build
-/home/steamcmd.sh +login $STEAM_ACCOUNT $STEAM_PASSWORD +run_app_build_http ./scripts/app_build.vdf +quit
+/home/steamcmd.sh \
+    +login $STEAM_ACCOUNT $STEAM_PASSWORD \
+    +run_app_build_http ./scripts/app_build.vdf \
+    +quit
+
+set +x
+
+# Check there is no error
+if [ -f $STEAM_CMD_STDERR_FILE ]; then
+    cat $STEAM_CMD_STDERR_FILE
+    exit 1
+fi
